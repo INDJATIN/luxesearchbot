57b37e2ba03ef048a1ad1f209794d72a2b941749
INDJATIN <130268528+INDJATIN@users.noreply.github.com>
2024-05-25 08:25:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-ADMIN = environ.get('ADMIN', '') #seperate by id # Replace this with your group chat ID\
+API_ID = int(environ.get('API_ID', '27663778'))
+API_HASH = environ.get('API_HASH', '2e751726850d421b67021e0edf85cbc6')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7192994294:AAExKaBkE_kACZn3-Wy6Jb2i8O3WMDZ6NgQ')
+ADMIN = environ.get('ADMIN', '1164156857') #seperate by id # Replace this with your group chat ID\
 
